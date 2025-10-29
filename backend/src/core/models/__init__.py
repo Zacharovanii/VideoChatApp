346aff8057a3a9fa820bf491dbd16e7f615abb76
Zacharovanii <zacharovaniiDev@proton.me>
2025-10-29 03:33:15 +0300

    add fastapi app template

---
+++ backend/src/core/models/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ("db_helper", "Base", "User")
+from .db_helper import db_helper
+from .base import Base
+from .user import User
---
+++ backend/src/main.py
@@ -0,0 +1,22 @@
+from contextlib import asynccontextmanager
+
+import uvicorn
+from fastapi import FastAPI
+
+from core.config import settings
+
+from api import router as api_router
+from core.models import db_helper
+
+
+@asynccontextmanager
+async def lifespan(fs_app: FastAPI):
+    yield
+    await db_helper.dispose()
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(api_router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host=settings.run.host, port=settings.run.port, reload=True)
